9ce3fbb0314bf3fec54612f7037e56ff1e224d5f
jan <crackerj2ck@proton.me>
2026-09-23 15:52:48 +0500

    feat: implement NoTasks component and update Plan route to display it

--- app/routes/plan.tsx
+++ app/routes/plan.tsx
@@ -1,3 +1,11 @@
+import { Container } from "@mantine/core";
+
+import { NoTasks } from "~/widgets/no-tasks";
+
 export default function Plan() {
-    return <div></div>
+    return (
+        <Container maw={720}>
+            <NoTasks />
+        </Container>
+    );
 }
---
+++ app/widgets/no-tasks/index.ts
@@ -0,0 +1,1 @@
+export { NoTasks } from "./ui/NoTasks";
