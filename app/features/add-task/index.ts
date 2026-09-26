54e214f1819c62c013bfaa3416b866dd2be46fd0
jan <crackerj2ck@proton.me>
2026-09-26 12:25:05 +0500

    feat: add TaskEntry component for task input and session control

---
+++ app/features/add-task/index.ts
@@ -0,0 +1,1 @@
+export { TaskEntry } from "./ui/TaskEntry";
---
+++ app/features/add-task/ui/TaskEntry.tsx
@@ -0,0 +1,40 @@
+import { ActionIcon, Group, Kbd, Text, TextInput } from "@mantine/core";
+import { MinusIcon, PlusIcon } from "@phosphor-icons/react";
+
+export function TaskEntry() {
+    return (
+        <Group my="lg" gap="sm">
+            <TextInput
+                flex={1}
+                variant="filled"
+                size="md"
+                placeholder="Add a task..."
+                aria-label="New task"
+                rightSection={
+                    <Text size="xs">
+                        <Kbd size="xs">N</Kbd>
+                    </Text>
+                }
+                rightSectionWidth={48}
+            />
+
+            <Group gap="lg" bg="gray.1" bdrs="md" h={42} px="xs">
+                <ActionIcon
+                    variant="transparent"
+                    size="lg"
+                    aria-label="Decrease planned sessions"
+                >
+                    <MinusIcon size={16} />
+                </ActionIcon>
+                <Text>-</Text>
+                <ActionIcon
+                    variant="transparent"
+                    size="lg"
+                    aria-label="Increase planned sessions"
+                >
+                    <PlusIcon size={16} />
+                </ActionIcon>
+            </Group>
+        </Group>
+    );
+}
