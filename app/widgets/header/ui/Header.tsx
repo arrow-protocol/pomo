f9d11d9d02426f61a65c13fd33784bcb96fff240
jan <crackerj2ck@proton.me>
2026-09-26 12:25:21 +0500

    style: refactor Header component layout and styling for improved UI consistency

--- app/widgets/header/ui/Header.tsx
+++ app/widgets/header/ui/Header.tsx
@@ -1,25 +1,38 @@
-import { ActionIcon, Box, Container, Flex, Group, Text } from "@mantine/core";
+import {
+    ActionIcon,
+    Box,
+    Button,
+    Container,
+    Flex,
+    Group,
+    Text,
+} from "@mantine/core";
 import { GearIcon } from "@phosphor-icons/react";
 
 export function Header() {
     return (
-        <Container>
-            <Flex h={64} justify="space-between" align="center">
-                <Group>
-                    <Box bg="red.9" w={12} h={12} bdrs={100} />
-                    <Text size="lg" fw={500}>
+        <Container fluid px={{ base: "md", sm: "xl" }}>
+            <Flex h={60} justify="space-between" align="center">
+                <Group gap="xs">
+                    <Box bg="red.8" w={10} h={10} bdrs="xl" />
+                    <Text size="md" fw={500}>
                         Pomodoro
                     </Text>
                 </Group>
-                <ActionIcon
-                    variant="subtle"
-                    color="gray"
-                    size="lg"
-                    radius="lg"
-                    aria-label="Settings"
-                >
-                    <GearIcon width={20} height={20} />
-                </ActionIcon>
+                <Group gap="xs">
+                    <Button variant="subtle" color="gray" size="sm">
+                        End day
+                    </Button>
+                    <ActionIcon
+                        variant="subtle"
+                        color="gray"
+                        size="lg"
+                        radius="lg"
+                        aria-label="Settings"
+                    >
+                        <GearIcon width={20} height={20} />
+                    </ActionIcon>
+                </Group>
             </Flex>
         </Container>
     );
